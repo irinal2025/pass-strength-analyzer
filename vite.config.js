b90da2ceb3a81193ec5a97ccc0c81344e46cb775
Irina Lisovskaja <179472627+irinal2025@users.noreply.github.com>
2025-04-02 04:03:26 +0300

    Configure Vite build output directory

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    outDir: 'dist', // Varmistaa, että kaikki menee distiin, mutta ei tarvitse erillistä /dist/ polkua
+  }
 })
