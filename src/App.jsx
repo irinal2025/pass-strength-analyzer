49ebe80f61c86e137e314887592482406e897330
Irina Lisovskaja <179472627+irinal2025@users.noreply.github.com>
2025-04-02 14:39:46 +0300

    Refactor project description

--- src/App.jsx
+++ src/App.jsx
@@ -24,20 +24,6 @@ function App() {
         </div>
         <PasswordValidator />
 
-        <div className="description">
-          <p>
-            In this project, I&apos;ve developed a password strength checker that demonstrates two different methods of evaluating password strength. The primary goal of this project is to help users understand how strong their passwords are and what factors contribute to their security.
-          </p>
-          <ul>
-            <li>
-              <strong>Custom Password Strength Meter:</strong>
-              I use the <code>zxcvbn</code> library to evaluate the password strength, taking into account dictionary words, common passwords, and password predictability. Additionally, I’ve incorporated custom logic to ensure that the password meets a predefined pattern (e.g., length, character variety). The strength meter provides dynamic visual feedback as the user types. The password strength <strong><i>never</i></strong> can be &quot;Good&quot; or &quot;Strong&quot; if the password doesn&apos;t meet the specified pattern.
-            </li>
-            <li>
-              <strong>React Password Strength Bar (with zxcvbn):</strong> The second method utilizes the <code>react-password-strength-bar</code> component to show a visual strength bar, which is updated based on the password strength evaluated by the <code>zxcvbn</code> library. This provides a simpler way for users to see how strong their password is at a glance, with a clear visual indicator.  Additionally, I use custom logic to ensure the password meets a predefined pattern and show errors if the password doesn&apos;t meet the specified pattern.
-            </li>
-          </ul>
-        </div>
 
         <div className="instructions">
           <h2>How to Create a Strong Password</h2>
@@ -54,7 +40,30 @@ function App() {
           <h4>Example of a very strong password:</h4>
           <p><code>!B7#cL$9aXp9M3$#</code></p>
           <p>It combines uppercase letters, lowercase letters, numbers, and special characters, making it difficult to guess.</p>
+        </div>        
+
+        <div className="description">
+          <h2>About This Project</h2>
+          <p>
+            In this project, I&apos;ve developed a password strength checker that demonstrates two different methods of evaluating password strength. The primary goal of this project is to help users understand how strong their passwords are and what factors contribute to their security.
+          </p>
+
+          <ul>
+            <li>
+              <strong>Custom Password Strength Meter:</strong> I use the <code>zxcvbn</code> library to evaluate the password strength, taking into account dictionary words, common passwords, and password predictability. Additionally, I’ve incorporated custom logic to ensure that the password meets a predefined pattern (e.g., length, character variety). The strength meter provides dynamic visual feedback as the user types. The password strength <strong><i>never</i></strong> can be &quot;Good&quot; or &quot;Strong&quot; if the password doesn&apos;t meet the specified pattern.
+            </li>
+            <li>
+              <strong>React Password Strength Bar (with zxcvbn):</strong> The second method utilizes the <code>react-password-strength-bar</code> component to show a visual strength bar, which is updated based on the password strength evaluated by the <code>zxcvbn</code> library. This provides a simpler way for users to see how strong their password is at a glance, with a clear visual indicator.  Additionally, I use custom logic to ensure the password meets a predefined pattern and show errors if the password doesn&apos;t meet the specified pattern.
+            </li>
+          </ul>
+          
+          
+          <p>
+            You can check out the source code on <a href="https://github.com/irinal2025/pass-strength-analyzer" target="_blank" rel="noopener noreferrer">GitHub</a>.
+          </p>
         </div>
+
+
 
         <div className="info-text">
           <p>This is a demo of a password strength checker built with React, Vite, and the zxcvbn library. Feel free to try it out and test your passwords!<span role="img" aria-label="smile">😊</span></p>
@@ -62,7 +71,7 @@ function App() {
       </main>
 
       <footer>
-        <p>&copy; {currentYear} Irina L. All rights reserved. Check out the code on <a href="https://github.com/irinal2025/pass-strength-analyzer" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
+        <p>&copy; {currentYear} Irina L. All rights reserved.</p>
       </footer>
     </>
   )
