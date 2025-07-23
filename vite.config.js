df68e5f3bc1de7f8eac046f1f7acd0b9a29fddd8
Irina Lisovskaja <179472627+irinal2025@users.noreply.github.com>
2025-07-23 18:51:29 +0300

    Update package.json and vite.config.js: add gh-pages and vite-plugin-gh-pages dependencies, and configure deployment settings

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  //base: '/', // Azure Static Web Apps 
+  base: '/pass-strength-analyzer/', // GitHub-repo
   build: {
     sourcemap: true, // Lisätään sourcemap-tiedostot buildiin. Tämä on hyödyllistä, jos halutaan debuggata tuotantoversiota.
     outDir: 'dist', // Varmistetaan, että kaikki menee distiin, mutta ei tarvita erillistä /dist/ -polkua.
